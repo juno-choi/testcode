75145030909301651fec9d0f311e6db99d26af09
juno-choi <ililil9482@naver.com>
2022-06-29 10:24:18 +0900

    테스트를 조건에 따라 실행하기

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 * [JUnit5](https://velog.io/@ililil9482/JUnit5)
 * [Assertions](https://velog.io/@ililil9482/Assertion)
+* [테스트를 조건에 따라 실행하기](https://velog.io/@ililil9482/%ED%85%8C%EC%8A%A4%ED%8A%B8%EB%A5%BC-%EC%A1%B0%EA%B1%B4%EC%97%90-%EB%94%B0%EB%9D%BC-%EC%8B%A4%ED%96%89%ED%95%98%EA%B8%B0)
--- src/test/java/com/example/testcode/IfTest.java
+++ src/test/java/com/example/testcode/IfTest.java
@@ -1,4 +1,76 @@
 package com.example.testcode;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
 public class IfTest {
+
+    static String VALUE = "TEST";
+
+    @Test
+    @DisplayName("조건 테스트")
+    void assumeTest(){
+        assumeTrue("실행 안됨".equals(VALUE));
+        System.out.println("조건 테스트");
+    }
+
+    @Test
+    @DisplayName("무조건 실행")
+    void test(){
+        System.out.println("무조건 실행되는 테스트");
+    }
+
+    @Test
+    @DisplayName("조건 테스트2")
+    void assumeThat(){
+        assumingThat("실행 안됨".equals(VALUE), ()-> System.out.println("실행"));
+        assumingThat("TEST".equals(VALUE), ()-> System.out.println("실행"));
+    }
+
+    @Test
+    @DisplayName("window만 실행")
+    @EnabledOnOs(value = OS.WINDOWS)
+    void window(){
+        System.out.println("윈도우 테스트입니다.");
+    }
+
+    @Test
+    @DisplayName("window 외 실행")
+    @DisabledOnOs(value = OS.WINDOWS)
+    void disabledWindow(){
+        System.out.println("윈도우에서는 실행하지 않을 테스트입니다..");
+    }
+
+
+    @Test
+    @DisplayName("linux, max만 실행")
+    @EnabledOnOs(value = {OS.MAC, OS.LINUX})
+    void mac(){
+        System.out.println("mac, linux 테스트입니다.");
+    }
+
+    @Test
+    @DisplayName("java11에서만 실행")
+    @EnabledOnJre(value = {JRE.JAVA_11})
+    void java11(){
+        System.out.println("java 11에서만 실행");
+    }
+
+    @Test
+    @DisplayName("java11 외 실행")
+    @DisabledOnJre(value = {JRE.JAVA_11})
+    void disabledJava11(){
+        System.out.println("java 11 외 실행");
+    }
+
+    @Test
+    @DisplayName("java11 외 실행")
+    @EnabledOnJre(value = {JRE.JAVA_8, JRE.JAVA_18})
+    void disabledJava8(){
+        System.out.println("java 8, 11에서만 실행");
+    }
 }
